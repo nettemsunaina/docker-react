c09e52cbb66ab23aa5878846bfff951c35c5233a
sunaina nettem <sunainanettem@Chowdaris-MacBook-Air-3.local>
2020-08-04 10:40:18 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           hiiii i am changed!
+           text has been changed!
         </p>
         <a
           className="App-link"
